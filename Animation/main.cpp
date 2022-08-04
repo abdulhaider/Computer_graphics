07d6b8f24b17ad55d25b91e7f1c57751ccee4933
abdulhaider <84699784+abdulhaider@users.noreply.github.com>
2022-08-05 00:53:51 +0600

    Add files via upload

---
+++ Animation/main.cpp
@@ -0,0 +1,131 @@
+
+
+#include <GL/gl.h>
+#include <GL/glut.h>
+
+
+GLfloat position = 0.0f;
+GLfloat position2 = 0.0f;
+GLfloat speed = 0.1f;
+
+void update(int value) {
+
+
+if(position < -1.0)
+        position = 1.0f;
+
+    position -= speed;
+
+
+	glutPostRedisplay();
+
+
+	glutTimerFunc(100, update, 0);
+}
+
+
+
+void update2(int value) {
+
+if(position > 1.0)
+        position = -1.0f;
+
+    position += speed;
+
+
+	glutPostRedisplay();
+
+
+	glutTimerFunc(100, update2, 0);
+}
+
+void init() {
+   glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+}
+
+void SpecialInput(int key, int x, int y)
+{
+switch(key)
+{
+case GLUT_KEY_UP:
+
+glutTimerFunc(100, update2, 0);
+break;
+case GLUT_KEY_DOWN:
+
+   glutTimerFunc(100, update, 0);
+break;
+case GLUT_KEY_LEFT:
+    speed =0.0f;
+
+break;
+case GLUT_KEY_RIGHT:
+    speed =0.0f;
+
+break;
+}
+glutPostRedisplay();
+}
+void display() {
+   glClear(GL_COLOR_BUFFER_BIT);
+   glLoadIdentity();
+
+
+
+glPushMatrix();
+glTranslatef(position,position2, 0.0f);
+
+   glBegin(GL_QUADS);
+      glColor3f(0.0f, 1.0f, 0.0f);
+      glVertex2f(-0.2f, -0.2f);
+      glVertex2f( 0.2f, -0.2f);
+      glVertex2f( 0.2f,  0.2f);
+      glVertex2f(-0.2f,  0.2f);
+   glEnd();
+   glBegin(GL_TRIANGLES);
+   glColor3f(0.0f, 0.0f, 1.0f);
+   glVertex2f(0.2f,-0.2f);
+   glVertex2f(0.5f, 0.0f);
+   glVertex2f(0.2f, 0.2f);
+   glEnd();
+   glBegin(GL_TRIANGLES);
+   glColor3f(0.0f, 0.0f, 1.0f);
+   glVertex2f(-0.2f,-0.2f);
+   glVertex2f(-0.5f, 0.0f);
+   glVertex2f(-0.2f, 0.2f);
+   glEnd();
+   glBegin(GL_TRIANGLES);
+   glColor3f(1.0f, 0.0f, 0.0f);
+   glVertex2f(-0.2f,-0.2f);
+   glVertex2f(0.2f,-0.2f);
+   glVertex2f(0.0f, -0.5f);
+   glEnd();
+   glBegin(GL_TRIANGLES);
+   glColor3f(1.0f, 0.0f, 0.0f);
+   glVertex2f(-0.2f,0.2f);
+   glVertex2f(0.2f,0.2f);
+   glVertex2f(0.0f,0.5f);
+   glEnd();
+
+
+
+glPopMatrix();
+
+
+
+   glFlush();
+}
+
+
+
+int main(int argc, char** argv) {
+   glutInit(&argc, argv);
+   glutInitWindowSize(320, 320);
+   glutInitWindowPosition(50, 50);
+   glutCreateWindow("Basic Animation");
+   glutDisplayFunc(display);
+   init();
+   glutSpecialFunc(SpecialInput);
+   glutMainLoop();
+   return 0;
+}
